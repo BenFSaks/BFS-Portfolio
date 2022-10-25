b82f29480e133d5a5e401627e3cce2dc1bd94980
BenFSaks <83617955+BenFSaks@users.noreply.github.com>
2022-10-25 18:40:25 -0400

    Classwork for 10/25/22

--- portfolio-app/src/pages/GitHub.jsx
+++ portfolio-app/src/pages/GitHub.jsx
@@ -6,36 +6,28 @@ export default function GitHub() {
 
     const octokit = new Octokit({ auth: process.env.gh_personal_access_token });
     const [user,setUser] = useState([])
-    console.log("hey")
+    const [lang,setLanguage] = useState([])
     useEffect(() => {
         const init = async () =>{
             // const {data } = await octokit.rest.users.getAuthenticated();
             const {data} = await octokit.rest.repos.listForUser({username: "BenFSaks"})
-            console.log("Hello, %s", JSON.stringify(data , null, 4));
+            // console.log("Hello, %s", JSON.stringify(data , null, 4));
             const userInfo = []
+            const languageInfo = []
             for (let i in data){
                 // const res = await axios.get(data[i].languages_url)
                 userInfo.push(data[i].name)
+                const api = `https://api.github.com/repos/BenFSaks/${data[i].name}/languages`
+                const res = await fetch(api)
+                const languages = await res.json()
+                languageInfo.push(languages)
+                for (let language in languages){
+                    // console.log(`language ${language} = ${languages[language]}`)
+                }
                 // userInfo.push(<div className="project-card"><h1 >{data[i].name}</h1></div>)
             }
+            setLanguage(languageInfo)
             setUser(userInfo)
-            // const json_getAllKeys = data => (
-            // data.reduce((keys, obj) => (
-            //         keys.concat(Object.keys(obj).filter(key => (
-            //         keys.indexOf(key) === -1))
-            //         )
-            //     ), [])
-            // )
-            // const json = JSON.parse(data.reduce)
-            console.log(userInfo)
-            // for (let info in data){
-            //     for(let key in data[info]){
-            //         if (data[info][key] != Object)
-            //             console.log(data[info][key])
-            //             userInfo.push(<li>{key} : {data[info][key]}</li>)
-
-            //     }
-            // }
 
             
 
@@ -49,11 +41,38 @@ export default function GitHub() {
         <>
             
             <div className="container">
-                {user.map((project) =>{
+                {user.map((project, indx) =>{
+
+                    let total = 0
+                    for (let i in lang[indx]){
+                        total += lang[indx][i]
+                    }
+                    console.log("total" + total)
+                    let langList = ""
+                    for (let i in lang[indx]){
+                        let perc = Math.floor((lang[indx][i] / total) * 100)
+                        if (perc == 0){
+                            perc = (lang[indx][i] / total) * 100
+                            perc = perc.toFixed(2)
+                        }
+                        langList += `${i} = ${perc}% `
+
+
+
+                    }
+
+                    let projectLink = `https://github.com/BenFSaks/${project}`
+                    if (project === "BenFSaks"){
+                        projectLink =  `https://github.com/BenFSaks/`
+                    }
                     return(
                         <div >
                             <p>{project}</p>
-                            <p>Lorem ipsum dolor sit amet.</p>
+                            <a href={projectLink}>View Here</a>
+                            <div>
+                                {langList}
+                            </div>
+
                         </div>
                     )
 
