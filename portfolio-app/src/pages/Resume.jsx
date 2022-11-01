dbb08fd39e747f7652cb2bb49b069d17b3a3197b
BenFSaks <83617955+BenFSaks@users.noreply.github.com>
2022-11-01 19:05:40 -0400

    resume page with form

---
+++ portfolio-app/src/pages/Resume.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="Name"></label>
+                <input type="text" />
+                <label htmlFor="Email"></label>
+                <input type="email" name="" id="" />
+            </form>
+        </div>
+    );
+}
+
+export default Resume;
--- portfolio-app/src/pages/Socials.jsx
+++ portfolio-app/src/pages/Socials.jsx
@@ -7,12 +7,8 @@ function Socials() {
     <div class="content">
         <Nav></Nav>
         <div class="images">
-            <div>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/2560px-YouTube_full-color_icon_%282017%29.svg.png" alt="" />
-            </div>
-            <div>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png" alt="" />
-            </div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/2560px-YouTube_full-color_icon_%282017%29.svg.png" alt="" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png" alt="" />
 
         </div>
     </div>
